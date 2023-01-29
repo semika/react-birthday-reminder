c0b0873aeed7b0b2f392619e122d75f9cb11f259
semikas <semika.s@aeturnum.com>
2023-01-29 10:05:43 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -6,27 +6,63 @@ import './App.css';
 import List from './List';
 import React, {useState} from 'react';
 
-function App() {
-  // In this project, 'App' component maintain an state.
-  // The state is list of people. 
-  const [people,setPeople] = useState(data)
-  return (
-    <main>
-      <section className='container'>
-        <h3>{people.length} bithdays today.</h3>
+//##################################################################
+// The following commented code shows how to use functional approch.
+//##################################################################
 
-        {/* This is how, we pass data into a Component. This data can be
-        accessed by the List component via props attribute */}
+// function App() {
+//   // In this project, 'App' component maintain an state.
+//   // The state is list of people. 
+//   const [people,setPeople] = useState(data)
+//   return (
+//     <main>
+//       <section className='container'>
+//         <h3>{people.length} bithdays today.</h3>
 
-        <List people={people}/>
+//         {/* This is how, we pass data into a Component. This data can be
+//         accessed by the List component via props attribute */}
 
-        {/* The following shows how to define inline function */}
+//         <List people={people}/>
 
-        <button onClick={() => { setPeople([]) }}>Clear All</button>
-      </section>
-    </main>
+//         {/* The following shows how to define inline function */}
+
+//         <button onClick={() => { setPeople([]) }}>Clear All</button>
+//       </section>
+//     </main>
     
-  );
+//   );
+// }
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      people : data
+    }
+  }
+
+  render() {
+    
+    const people = this.state.people;
+    
+    return(
+        <main>
+          <section className='container'>
+            <h3>{people.length} bithdays today.</h3>
+
+            {/* This is how, we pass data into a Component. This data can be
+            accessed by the List component via props attribute */}
+
+            <List people={people}/>
+
+            {/* The following shows how to define inline function */}
+
+            <button onClick={() => { this.setState({people:[]}) }}>Clear All</button>
+          </section>
+        </main> 
+      );
+  };  
 }
 
 export default App;
